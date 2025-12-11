3ae45391138077e2c324aeaf43eb435a0764801c
primidac <primidac@gmail.com>
2025-12-11 23:43:07 +0100

    PWA ADDED and fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,19 +39,22 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon_io/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon_io/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon_io/favicon.ico', sizes: 'any' }
+      { url: '/bitsaveicon.jpg', sizes: 'any', type: 'image/jpeg' }
     ],
     apple: [
-      { url: '/favicon_io/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+      { url: '/bitsaveicon.jpg', sizes: '180x180', type: 'image/jpeg' }
     ],
     other: [
-      { url: '/favicon_io/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/favicon_io/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
+      { url: '/bitsaveicon.jpg', sizes: '192x192', type: 'image/jpeg' },
+      { url: '/bitsaveicon.jpg', sizes: '512x512', type: 'image/jpeg' }
     ]
   },
-  manifest: '/favicon_io/site.webmanifest'
+  manifest: '/favicon_io/site.webmanifest',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'BitSave',
+  },
 }
 
 export const viewport: Viewport = {
